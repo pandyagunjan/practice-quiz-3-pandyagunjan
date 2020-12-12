af635e9b2e143b3a026041a59a59de49d1f46fe4
gunjan <pandya.gunjan@gmail.com>
2020-12-12 08:48:44 -0500

    little progress

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -4,19 +4,29 @@ package rocks.zipcode.io.quiz3.arrays;
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    String[][] board;
 
     public TicTacToe(String[][] board) {
+        this.board = board;
+
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+
+        return this.board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+    String[] newArray=new String[board.length];
+    for(int i=0 ; i <board.length ;i++)
+    {
+        String temp = board[i][value];
+        newArray[i]=temp;
+    }
+        return newArray;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
@@ -32,6 +42,6 @@ public class TicTacToe {
     }
 
     public String[][] getBoard() {
-        return null;
+        return this.board;
     }
 }
